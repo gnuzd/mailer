bbfd9628f6b912de5c1c8c1a97b7464e90d2f76b
Chris Nguyen <chris.dungnguyen@gmail.com>
2025-11-12 18:44:26 +0700

    feat: wrong env

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,14 +1,14 @@
 import type SMTPPool from 'nodemailer/lib/smtp-pool';
 
 export const config: Record<string, SMTPPool.Options> = {
-  atomarius: {
-    pool: true,
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: true,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  },
+	atomarius: {
+		pool: true,
+		host: process.env.ATOMARIUS_SMTP_HOST,
+		port: Number(process.env.ATOMARIUS_SMTP_PORT),
+		secure: true,
+		auth: {
+			user: process.env.ATOMARIUS_SMTP_USER,
+			pass: process.env.ATOMARIUS_SMTP_PASS
+		}
+	}
 };
